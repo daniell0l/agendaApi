db1da9fc2f4a4d97ed97c8ba303ce7697e2da329
daniell0l <daniel_l0l@hotmail.com>
2023-05-22 23:14:28 -0300

    main-feat: importando e utilizando o registe model no controller

--- src/controllers/registerController.js
+++ src/controllers/registerController.js
@@ -1,9 +1,10 @@
-const registerModel = require('../models/registerModel');
+const Register = require('../models/registerModel');
 
 exports.register = (req, res) => {
   res.render('register/register');
 };
 
 exports.access = (req, res) => {
-  res.send(req.body);
+  const register = new Register(req.body)
+  res.send(register.body);
 };
